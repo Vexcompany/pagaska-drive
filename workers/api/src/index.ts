b1d150fedd72dab5717b4339ee8844a5e59ec4cb
Vexcompany <vexcorporation43@gmail.com>
2026-07-30 17:09:43 +0700

    Create index.ts

---
+++ workers/api/src/index.ts
@@ -0,0 +1,8 @@
+import { handle } from "./router";
+import type { Env } from "./env";
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    return handle(request, env);
+  },
+};
